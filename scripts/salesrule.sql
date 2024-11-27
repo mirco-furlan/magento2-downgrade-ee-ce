3c6f6e432435042274eaa409c36c08a4e7c1b6c0
mirco-furlan <153541589+mirco-furlan@users.noreply.github.com>
2024-11-27 17:50:12 +0100

    Update salesrule.sql

--- scripts/salesrule.sql
+++ scripts/salesrule.sql
@@ -67,6 +67,9 @@ ALTER TABLE `salesrule_product_attribute`
     DROP COLUMN `row_id`;
 
 -- Salesrule
+ALTER TABLE `salesrule_label`
+    DROP FOREIGN KEY `SALESRULE_LABEL_ROW_ID_SALESRULE_ROW_ID`,
+    DROP COLUMN `row_id`;
 ALTER TABLE `salesrule`
     DROP FOREIGN KEY `SALESRULE_RULE_ID_SEQUENCE_SALESRULE_SEQUENCE_VALUE`,
     DROP COLUMN `row_id`,
